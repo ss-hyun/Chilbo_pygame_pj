50d92202c4dfdc1448986c39e64e770f78c25501
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-11-06 20:43:50 +0900

    Add character resize function for boss

--- stage1.py
+++ stage1.py
@@ -31,6 +31,9 @@ def user_start(ch, game):
 def boss_start(ch, game):
     ch.pos = [ (game.display_size[0]-ch.size[ch.curr_state][0])/2, ch.size[ch.curr_state][1]-100 ]
 
+def boss_resize(ch):
+    pass
+
 def stage1(name, path, fps, speed):
     bg_image = pygame.image.load(path + "/image/stage1_background.jpg")
     # character info : (name, relative path list, function list, group)
@@ -39,7 +42,7 @@ def stage1(name, path, fps, speed):
     # # function list : A function of all actions that can be done as a character, including initialization.
     # #                 [move, positioning, attack] - if it doesn't exist -> None
     # # group : There are user groups(0) and monster groups(1) in the game.
-    ch_info_list = [ ("user", [ "/image/character_r.jpg", "/image/character_l.jpg" ], [ move_user, user_start, None ], 0),
-                     ("boss", [ "/image/exboss.svg" ], [ None, boss_start, None ], 1) ]
+    ch_info_list = [ ("user", [ "/image/character_r.jpg", "/image/character_l.jpg" ], [ move_user, user_start, None, None ], 0),
+                     ("boss", [ "/image/exboss.svg" ], [ None, boss_start, None, boss_resize ], 1) ]
     game = stage_template.Stage(name, 1, path, fps, speed, bg_image, ch_info_list)
     game.run()
