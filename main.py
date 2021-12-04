0876fab16f71e84efa8a78ec05e7a1985dfd6f9a
ss-hyun <yunatlsghk6@ajou.ac.kr>
2021-12-05 00:04:45 +0900

    Update Game name

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ bt_image_st1_down = pygame.image.load(path + "/image/start_button_stage1_down.jp
 
 # setting
 display_size = bg_image.get_rect().size
-name = "game"
+name = "GUN KID"
 speed = 100
 running = True
 gname_pos = ((display_size[0]-gname_image.get_rect().size[0])/2, (display_size[1]-gname_image.get_rect().size[1])/2)
